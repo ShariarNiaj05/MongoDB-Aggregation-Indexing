1ffcb98a721eaf456143780debbd1303b0e8c7b9
Shariar Islam <shariarn85@gmail.com>
2024-05-11 22:40:14 +0600

    Explore More About  &

--- index.ts
+++ index.ts
@@ -101,3 +101,39 @@ db.test.aggregate(
 )
 
 */
+
+
+/***
+ * 
+ * 6-4 
+ * Explore More About $Group & $Project
+ * 
+ * db.test.aggregate(
+    [
+        // stage 1 
+
+        {
+            $group: {
+                _id: null,
+                totalSalary: { $sum: "$salary" },
+                maxSalary: { $max: "$salary" },
+                minSalary: { $min: "$salary" },
+                avgSalary: { $avg: "$salary" },
+            }
+
+        },
+
+        // stage 2 
+        {
+            $project: {
+                totalSalary: 1,
+                maxSalary: 1,
+                minSalary: 1,
+                averageSalary: "$avgSalary", // Changing the name
+                rangeBetweenMaxAndMin: {$subtract: ["$maxSalary", "$minSalary"]}
+            }
+        }
+
+    ]
+)
+*/
