2e809862df52139280a40636321225379a4c3026
Shariar Islam <shariarn85@gmail.com>
2024-05-11 19:21:34 +0600

    intro to aggregation

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('hello');
+/****
+ * ==> 6-0 Intro The Powerful Aggregation Framework
+ * 
+ *  Aggregation: Is a way of processing a large number of documents in a collection by means of passing them through different stages. 
+ * The stages is known as "PIPELINE"
+ * 
+ * */
+
