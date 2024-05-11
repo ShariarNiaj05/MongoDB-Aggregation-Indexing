a1db8f2edef1f1656611ff2c0023c19ab8949bfa
Shariar Islam <shariarn85@gmail.com>
2024-05-11 20:22:32 +0600

    ,  ,  Aggregation Stage

--- index.ts
+++ index.ts
@@ -21,3 +21,32 @@
     ]
 )
 */
+
+
+/****
+ * 6-2 
+ * 
+ * NOTE::: Focus on using less stage/ pipeline as it can slow down the execution time!
+ * 
+ * 
+ * $AddFields , $Out , $Merge Aggregation Stage
+ * 
+ * 
+ * db.test.aggregate(
+    [
+        // stage = 1
+        { $match: { gender: "Male" } },
+
+        // stage 2 
+        { $addFields: { courseLevel: "Level 2", eduTech: "Programming Hero" } },
+
+        // stage 3
+        { $project: { gender: 1, courseLevel: 1, eduTech: 1, age: 1, skills: 1 } },
+        
+        // //stage 4
+        // { $out: "courseStudnet"} //create new collection with the given new property 
+        {$merge: { into: { db: "test", coll: "newData" } }} //update the present collection with the given new property 
+    ]
+)
+
+*/
