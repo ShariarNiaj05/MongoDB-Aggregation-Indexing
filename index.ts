bd59e142a55c4d065105bdf628be991c63e8d068
Shariar Islam <shariarn85@gmail.com>
2024-05-11 19:39:12 +0600

    ,  Aggregation Stage

--- index.ts
+++ index.ts
@@ -6,3 +6,18 @@
  * 
  * */
 
+/***
+ * 
+ * 6-1 
+ * $Match and $Project Aggregation Stage
+ * 
+ * db.test.aggregate(
+    [
+        // stage = 1
+        { $match: { gender: "Male", age: { $lt: 30 } } },
+        
+        // stage 2 
+        {$project: {name: 1, email: 1, age: 1}} // project should be used at the very last stage 
+    ]
+)
+*/
